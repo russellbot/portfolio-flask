3892f3b72cf572d65ce40532dfb0aaaae01eca2b
Russell Hart <russellbot@gmail.com>
2021-03-14 18:54:45 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'this is my dog'
