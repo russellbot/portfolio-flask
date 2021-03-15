7891b481a571232ff49b431e47d9b79f16e90e9b
Russell Hart <russellbot@gmail.com>
2021-03-14 21:20:12 -0400

    changed routes to remove repeated code

--- server.py
+++ server.py
@@ -7,26 +7,6 @@ print(__name__)
 def my_home():
     return render_template('index.html')
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
-
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
